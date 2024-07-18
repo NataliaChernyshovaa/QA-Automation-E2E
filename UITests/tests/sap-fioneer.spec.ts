fc96ed847d0781db587160fc9dfa12b0c07b5ed1
Natalia Ch <natcernysova@gmail.com>
2024-07-18 19:07:56 +0200

    CircleCI configuration

--- UITests/tests/sap-fioneer.spec.ts
+++ UITests/tests/sap-fioneer.spec.ts
@@ -1,46 +1,57 @@
-import  { test } from "../src/fixture/fixture"
-import { expect } from '@playwright/test'
+import { test } from '../src/fixture/fixture';
+import { expect } from '@playwright/test';
 import { NAVIGATION_ITEMS } from '../src/support/types';
 import { contactUrl, esgKpiEngineUrl } from '../src/support/constants';
 import { unvalidEmail } from '../src/support/emailTestData';
 import { errorValidatinEmailMessage } from '../src/support/errorMessages';
-import { blogPageTitle } from "../src/support/regExr";
-
+import { blogPageTitle } from '../src/support/regExr';
 
 test.describe('Official Site Tests', () => {
-  
-    test.beforeEach(async ({ homePage }) => {
-      await homePage.visitPage()
-    })
-  
-    test ('Should verify if Get in touch button has a yellow color', async ( {homePage} ) => {
-        const color = await homePage.getInToutchButton.evaluate((item) => {
-        return window.getComputedStyle(item).getPropertyValue("background-color")
-      })
-  
-      expect(color).toBe('rgb(255, 212, 60)')
+  test.beforeEach(async ({ homePage }) => {
+    await homePage.visitPage();
+  });
 
+  test('Should verify if Get in touch button has a yellow color', async ({
+    homePage,
+  }) => {
+    const color = await homePage.getInToutchButton.evaluate((item) => {
+      return window.getComputedStyle(item).getPropertyValue('background-color');
     });
 
-    test (`Should verify if a user has been redirected to the ${esgKpiEngineUrl} page`, async ( {homePage, esgkpiEnginePage} ) => {
-        await homePage.navigationBar.getNavigationItemByText(NAVIGATION_ITEMS.FINANCE_ESG).hover()
-        await homePage.ESGKPIEngineLink.click()
-      
-        expect(await homePage.getUrl()).toBe(esgKpiEngineUrl);
-        expect (esgkpiEnginePage.h1ElementESGKPIEngine).toBeVisible
-      });
+    expect(color).toBe('rgb(255, 212, 60)');
+  });
 
-      test (`Should Verify if validation message will appear if email is unvalid`, async ( {homePage, contactFormPage} ) => {
-        await homePage.getInToutchButton.click();
-        expect(await contactFormPage.getUrl()).toBe(contactUrl);
-        
-        await contactFormPage.emailField.pressSequentially(unvalidEmail)
-        await expect (contactFormPage.emailErrorLabel).toContainText(errorValidatinEmailMessage)
-      }); 
+  test(`Should verify if a user has been redirected to the ${esgKpiEngineUrl} page`, async ({
+    homePage,
+    esgkpiEnginePage,
+  }) => {
+    await homePage.navigationBar
+      .getNavigationItemByText(NAVIGATION_ITEMS.FINANCE_ESG)
+      .hover();
+    await homePage.ESGKPIEngineLink.click();
 
-      test ('Should display title of blogPage correctly', async ({ blogPage }) => {
-        await expect.poll(async () => {
-          return blogPageTitle.test(await blogPage.getTitle());
-        }).toBeTruthy();
-      });
-  })
+    expect(await homePage.getUrl()).toBe(esgKpiEngineUrl);
+    expect(esgkpiEnginePage.h1ElementESGKPIEngine).toBeVisible;
+  });
+
+  test(`Should Verify if validation message will appear if email is unvalid`, async ({
+    homePage,
+    contactFormPage,
+  }) => {
+    await homePage.getInToutchButton.click();
+    expect(await contactFormPage.getUrl()).toBe(contactUrl);
+
+    await contactFormPage.emailField.pressSequentially(unvalidEmail);
+    await expect(contactFormPage.emailErrorLabel).toContainText(
+      errorValidatinEmailMessage,
+    );
+  });
+
+  test('Should display title of blogPage correctly', async ({ blogPage }) => {
+    await expect
+      .poll(async () => {
+        return blogPageTitle.test(await blogPage.getTitle());
+      })
+      .toBeTruthy();
+  });
+});
